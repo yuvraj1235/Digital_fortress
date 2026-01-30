0c6b36ec8d490b4044fd93e4c42bdc7e1f74cc21
yuvraj1235 <reignitedyuvraj@gmail.com>
2026-01-30 18:38:32 +0530

    deployment

--- app/house/page.tsx
+++ app/house/page.tsx
@@ -1,15 +1,40 @@
-import CustomCursor from "@/components/CustomCursor";
-import IslandScene from "@/components/Island";
+"use client";
+
+import { useEffect, useState } from "react";
 import Navbar from "@/components/Navbar";
 import Panorama from "@/components/House";
 import BottomBar from "@/components/ShareIcon";
+import LoadingScreen from "@/components/LoadingPage";
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+  const [progress, setProgress] = useState(0);
+
+  // Simulated loading
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress((prev) => {
+        if (prev >= 100) {
+          clearInterval(interval);
+          setLoading(false);
+          return 100;
+        }
+        return prev + 2;
+      });
+    }, 40); // ~2s
+
+    return () => clearInterval(interval);
+  }, []);
+
+  if (loading) {
+    return <LoadingScreen progress={progress} />;
+  }
+
   return (
     <main className="flex min-h-screen items-center justify-center">
-      <Navbar></Navbar>
-      <Panorama/>
-     <BottomBar></BottomBar>
+      <Navbar />
+      <Panorama />
+      <BottomBar />
     </main>
   );
 }
--- app/house2/page.tsx
+++ app/house2/page.tsx
@@ -1,14 +1,39 @@
-import CustomCursor from "@/components/CustomCursor";
+"use client";
+
+import { useEffect, useState } from "react";
 import Navbar from "@/components/Navbar";
 import Panorama from "@/components/House2";
 import BottomBar from "@/components/ShareIcon";
+import LoadingScreen from "@/components/LoadingPage";
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress((prev) => {
+        if (prev >= 100) {
+          clearInterval(interval);
+          setLoading(false);
+          return 100;
+        }
+        return prev + 2;
+      });
+    }, 40);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  if (loading) {
+    return <LoadingScreen progress={progress} />;
+  }
+
   return (
     <main className="flex min-h-screen items-center justify-center">
-      <Navbar></Navbar>
-      <Panorama/>
-     <BottomBar></BottomBar>
+      <Navbar />
+      <Panorama />
+      <BottomBar />
     </main>
   );
 }
