8940993d66a4d416da75e3bc83bbca6d9a9bcc70
yuvraj1235 <reignitedyuvraj@gmail.com>
2026-01-30 18:07:26 +0530

    deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://13.53.205.86/:path*",
+
+        // 🔴 CHANGE to your REAL backend HTTPS URL
+      },
+    ];
+  },
 };
 
 export default nextConfig;
